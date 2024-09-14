4bb064527d889517880d46c31036b3cd77f530dd
autorejecttop <autorejecttop@outlook.com>
2024-09-14 22:46:51 +0800

    Added decision logic to my code using 'if', 'else', and 'else if' statements in C#

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,5 +1,22 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-Console.WriteLine(largerValue);
+if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration}.");
+    Console.WriteLine($"Renew now and save 10%!");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine($"Your subscription expires within a day!");
+    Console.WriteLine($"Renew now and save 20%!");
+}
+else if (daysUntilExpiration <= 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
