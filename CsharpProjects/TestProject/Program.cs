ecba773b44fb998a9a7bac65b5e57500ac49ea8d
autorejecttop <autorejecttop@outlook.com>
2024-09-15 16:29:01 +0800

    Branched the flow of code using the switch-case construct in C#

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,38 +1,129 @@
-﻿// // // bool flag = true;
-// // // int value;
+﻿// int employeeLevel = 100;
+// string employeeName = "John Smith";
 
-// // // if (true)
-// // // {
-// // //     value = 10;
-// // //     Console.WriteLine($"Inside the code block: {value}");
-// // // }
+// string title = "";
 
-// // // Console.WriteLine($"Outside the code block: {value}");
+// switch (employeeLevel)
+// {
+//     case 100:
+//     // title = "Junior Associate";
+//     // break;
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title = "Senior Manager";
+//         break;
+//     default:
+//         title = "Associate";
+//         break;
+// }
 
-// // bool flag = true;
-// // if (flag)
-// //     Console.WriteLine(flag);
+// Console.WriteLine($"{employeeName}, {title}");
 
-// string name = "steve";
-// if (name == "bob")
-//     Console.WriteLine("Found Bob");
-// else if (name == "steve")
-//     Console.WriteLine("Found Steve");
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+// if (product[0] == "01")
+// {
+//     type = "Sweat shirt";
+// }
+// else if (product[0] == "02")
+// {
+//     type = "T-Shirt";
+// }
+// else if (product[0] == "03")
+// {
+//     type = "Sweat pants";
+// }
 // else
-//     Console.WriteLine("Found Chuck");
+// {
+//     type = "Other";
+// }
 
-int[] numbers = [4, 8, 15, 16, 23, 42];
-int total = 0;
-bool found = false;
-
-foreach (int number in numbers)
+switch (product[0])
 {
-    total += number;
-    if (number == 42)
-        found = true;
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
 }
 
-if (found)
-    Console.WriteLine("Set contains 42");
+// if (product[1] == "BL")
+// {
+//     color = "Black";
+// }
+// else if (product[1] == "MN")
+// {
+//     color = "Maroon";
+// }
+// else
+// {
+//     color = "White";
+// }
 
-Console.WriteLine($"Total: {total}");
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+// if (product[2] == "S")
+// {
+//     size = "Small";
+// }
+// else if (product[2] == "M")
+// {
+//     size = "Medium";
+// }
+// else if (product[2] == "L")
+// {
+//     size = "Large";
+// }
+// else
+// {
+//     size = "One Size Fits All";
+// }
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
