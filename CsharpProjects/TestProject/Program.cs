717b62303391baaa242ae302f6e0571f8f9b2b91
autorejecttop <autorejecttop@outlook.com>
2024-09-15 13:26:07 +0800

    Evaluated Boolean expressions to make decisions in C#

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,23 +1,58 @@
-﻿/*
-  This code reverses a message, counts
-  the number of times a particular character
-  appears, then prints the results to the
-  console window.
-*/
-string originalMessage = "The quick brown fox jumps over the lazy dog.";
+﻿// // // // // // // Console.WriteLine("a" == "a");
+// // // // // // // Console.WriteLine("a" == "A");
+// // // // // // // Console.WriteLine(1 == 2);
 
-char[] message = originalMessage.ToCharArray();
-Array.Reverse(message);
+// // // // // // // string myValue = "a";
+// // // // // // // Console.WriteLine(myValue == "a");
 
-int letterCount = 0;
-foreach (char letter in message)
+// // // // // // // string value1 = " a";
+// // // // // // // string value2 = "A ";
+// // // // // // // Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+// // // // // // Console.WriteLine("a" != "a");
+// // // // // // Console.WriteLine("a" != "A");
+// // // // // // Console.WriteLine(1 != 2);
+
+// // // // // // string myValue = "a";
+// // // // // // Console.WriteLine(myValue != "a");
+
+// // // // // Console.WriteLine(1 > 2);
+// // // // // Console.WriteLine(1 < 2);
+// // // // // Console.WriteLine(1 >= 1);
+// // // // // Console.WriteLine(1 <= 1);
+
+// // // // string pangram = "The quick brown fox jumps over the lazy dog.";
+// // // // Console.WriteLine(pangram.Contains("fox"));
+// // // // Console.WriteLine(pangram.Contains("cow"));
+
+// // // string pangram = "The quick brown fox jumps over the lazy dog.";
+// // // Console.WriteLine(!pangram.Contains("fox"));
+// // // Console.WriteLine(!pangram.Contains("cow"));
+
+// // int saleAmount = 1001;
+// // // int discount = saleAmount > 1000 ? 100 : 50;
+
+// // Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+// Random coin = new();
+// int flip = coin.Next(0, 2);
+// Console.WriteLine(flip == 0 ? "heads" : "tails");
+
+string permission = "Admin|Manager";
+int level = 55;
+string message = "";
+
+if (permission.Contains("Admin"))
 {
-    if (letter == 'o')
-    {
-        letterCount++;
-    }
+    message = level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.";
+}
+else if (permission.Contains("Manager"))
+{
+    message = level >= 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.";
+}
+else
+{
+    message = "You do not have sufficient privileges.";
 }
 
-string newMessage = new String(message);
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {letterCount} times.");
+Console.WriteLine(message);
