de5866bf802ebcf24d230d25eeaebae354c8e4ad
autorejecttop <autorejecttop@outlook.com>
2024-09-16 17:13:27 +0800

    Added looping logic to my code using the do-while and while statements in C#

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,41 +1,138 @@
-﻿// // // // for (int i = 0; i < 10; i++)
+﻿// // // // // Random dice = new();
+
+// // // // // int heroHealth = 10;
+// // // // // int monsterHealth = 10;
+// // // // // bool isHeroTurn = true;
+
+// // // // // while (heroHealth > 0 && monsterHealth > 0)
+// // // // // {
+// // // // //     int damage = dice.Next(1, 11);
+
+// // // // //     if (isHeroTurn)
+// // // // //     {
+// // // // //         monsterHealth -= damage;
+// // // // //         Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+// // // // //     }
+// // // // //     else
+// // // // //     {
+// // // // //         heroHealth -= damage;
+// // // // //         Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health.");
+// // // // //     }
+
+// // // // //     isHeroTurn = !isHeroTurn;
+// // // // // }
+
+// // // // // if (heroHealth <= 0)
+// // // // //     Console.WriteLine("Monster wins!");
+// // // // // else if (monsterHealth <= 0)
+// // // // //     Console.WriteLine("Hero wins!");
+
+// // // // int hero = 10;
+// // // // int monster = 10;
+
+// // // // Random dice = new Random();
+
+// // // // do
 // // // // {
-// // // //     Console.WriteLine(i);
-// // // //     if (i == 7) break;
-// // // // }
+// // // //     int roll = dice.Next(1, 11);
+// // // //     monster -= roll;
+// // // //     Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
 
-// // // string[] names = ["Alex", "Eddie", "David", "Michael"];
-// // // for (int i = names.Length - 1; i >= 0; i--)
+// // // //     if (monster <= 0) continue;
+
+// // // //     roll = dice.Next(1, 11);
+// // // //     hero -= roll;
+// // // //     Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+// // // // } while (hero > 0 && monster > 0);
+
+// // // // Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+
+// // // string? readResult;
+// // // bool validEntry = false;
+// // // Console.WriteLine("Enter a string containing at least three characters:");
+// // // do
 // // // {
-// // //     Console.WriteLine(names[i]);
-// // // }
+// // //     readResult = Console.ReadLine();
+// // //     if (readResult != null)
+// // //     {
+// // //         if (readResult.Length >= 3)
+// // //             validEntry = true;
+// // //         else
+// // //             Console.WriteLine("Your input is invalid, please try again.");
+// // //     }
+// // // } while (!validEntry);
 
-// // string[] names = { "Alex", "Eddie", "David", "Michael" };
-// // foreach (var name in names)
+// // // int numericValue = 0;
+// // // bool validNumber = false;
+
+// // // validNumber = int.TryParse(readResult, out numericValue);
+
+// // string? rawInput;
+// // bool isInputValid = false;
+// // int numberValue = 0;
+
+// // Console.WriteLine("Enter an integer value between 5 and 10");
+
+// // do
 // // {
-// //     // Can't do this:
-// //     if (name == "David") name = "Sammy";
-// // }
+// //     rawInput = Console.ReadLine();
 
-// string[] names = ["Alex", "Eddie", "David", "Michael"];
+// //     if (rawInput == null)
+// //     {
+// //         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+// //         continue;
+// //     }
 
-// for (int i = 0; i < names.Length; i++)
-//     if (names[i] == "David")
-//         names[i] = "Sammy";
+// //     isInputValid = int.TryParse(rawInput, out numberValue);
 
-// foreach (string name in names)
-//     Console.WriteLine(name);
+// //     if (!isInputValid)
+// //     {
+// //         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+// //         continue;
+// //     }
 
-for (int i = 1; i <= 100; i++)
+// //     if (numberValue < 5 || numberValue > 10)
+// //     {
+// //         Console.WriteLine($"You entered {numberValue}. Please enter a number between 5 and 10.");
+// //         continue;
+// //     }
+// // } while (!isInputValid || numberValue < 5 || numberValue > 10);
+
+// // Console.WriteLine($"Your input value ({numberValue}) has been accepted.");
+
+// string[] roles = ["administrator", "manager", "user"];
+
+// Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+// string? userInput = Console.ReadLine();
+
+// while (userInput == null || !roles.Contains(userInput.Trim().ToLower()))
+// {
+//     Console.WriteLine($"The role name that you entered, \"{userInput}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+//     userInput = Console.ReadLine();
+// }
+
+// Console.WriteLine($"Your input value ({userInput}) has been accepted.");
+
+string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
+
+for (int i = 0; i < myStrings.Length; i++)
 {
-    Console.Write(i);
+    string myString = myStrings[i];
+    int periodLocation = myString.IndexOf('.');
 
-    if (i % 3 == 0 && i % 5 == 0)
-        Console.Write(" - FizzBuzz");
-    else if (i % 3 == 0)
-        Console.Write(" - Fizz");
-    else if (i % 5 == 0)
-        Console.Write(" - Buzz");
+    string mySentence;
 
-    Console.WriteLine();
+    while (periodLocation != -1)
+    {
+        mySentence = myString.Remove(periodLocation);
+
+        myString = myString.Substring(periodLocation + 1).TrimStart();
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
 }
