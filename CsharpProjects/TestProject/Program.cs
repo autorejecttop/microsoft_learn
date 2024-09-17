31cd106b0d3e4dd699279413141d4abb9be8c840
autorejecttop <autorejecttop@outlook.com>
2024-09-17 11:32:05 +0800

    Formatted alphanumeric data for presentation in C#

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,73 +1,85 @@
-﻿// // // // string[] pallets = ["B14", "A11", "B12", "A13"];
+﻿// // // // // // // // string first = "Hello";
+// // // // // // // // string second = "World";
+// // // // // // // // Console.WriteLine($"{first} {second}!");
+// // // // // // // // Console.WriteLine($"{second} {first}!");
+// // // // // // // // Console.WriteLine($"{first} {first} {first}!");
 
-// // // // Console.WriteLine("Sorted...");
-// // // // Array.Sort(pallets);
-// // // // foreach (string pallet in pallets)
-// // // //     Console.WriteLine($"-- {pallet}");
+// // // // // // // decimal price = 123.45m;
+// // // // // // // int discount = 50;
+// // // // // // // Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 
-// // // // Console.WriteLine();
-// // // // Console.WriteLine("Reversed...");
-// // // // Array.Reverse(pallets);
-// // // // foreach (string pallet in pallets)
-// // // //     Console.WriteLine($"-- {pallet}");
+// // // // // // decimal measurement = 123456.78912m;
+// // // // // // Console.WriteLine($"Measurement: {measurement:N} units");
 
-// // // string[] pallets = ["B14", "A11", "B12", "A13"];
-// // // Console.WriteLine();
+// // // // // decimal tax = .36785m;
+// // // // // Console.WriteLine($"Tax rate: {tax:P2}");
 
-// // // Array.Clear(pallets, 0, 2);
-// // // Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-// // // foreach (string pallet in pallets)
-// // //     Console.WriteLine($"-- {pallet}"); ;
+// // // // decimal price = 67.55m;
+// // // // decimal salePrice = 59.99m;
 
-// // // Console.WriteLine();
-// // // Array.Resize(ref pallets, 6);
-// // // Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+// // // // string yourDiscount = string.Format("You saved {0:C2} off the regular {1:C2} price. ", price - salePrice, price);
 
-// // // pallets[4] = "C01";
-// // // pallets[5] = "C02";
+// // // // yourDiscount += $"A discount of {(price - salePrice) / price:P2}!";
+// // // // Console.WriteLine(yourDiscount);
 
-// // // foreach (string pallet in pallets)
-// // //     Console.WriteLine($"-- {pallet}");
+// // // int invoiceNumber = 1201;
+// // // decimal productShares = 25.4568m;
+// // // decimal subtotal = 2750m;
+// // // decimal taxPercentage = 0.15825m;
+// // // decimal total = 3185.19m;
 
-// // // Console.WriteLine();
-// // // Array.Resize(ref pallets, 3);
-// // // Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+// // // Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// // // Console.WriteLine($"   Shares: {productShares:N3} Product");
+// // // Console.WriteLine($"     Sub Total: {subtotal:C}");
+// // // Console.WriteLine($"           Tax: {taxPercentage:P2}");
+// // // Console.WriteLine($"     Total Billed: {total:C}");
 
-// // // foreach (string pallet in pallets)
-// // //     Console.WriteLine($"-- {pallet}");
 
-// // string value = "abc123";
-// // char[] valueArray = value.ToCharArray();
-// // Array.Reverse(valueArray);
-// // // string result = new string(valueArray);
-// // string result = string.Join(",", valueArray);
-// // Console.WriteLine(result);
+// // string input = "Pad this";
+// // Console.WriteLine(input.PadLeft(12, '-'));
+// // Console.WriteLine(input.PadRight(12, '-'));
 
-// // string[] items = result.Split(",");
-// // foreach (string item in items)
-// //     Console.WriteLine(item);
+// string paymentId = "769C";
+// string payeeName = "Mr. Stephen Ortega";
+// string paymentAmount = "$5,000.00";
 
-// string pangram = "The quick brown fox jumps over the lazy dog";
-// string[] pangramArray = pangram.Split(" ");
+// string formattedLine = paymentId.PadRight(6);
+// formattedLine += payeeName.PadRight(24);
+// formattedLine += paymentAmount.PadLeft(10);
 
-// for (int i = 0; i < pangramArray.Length; i++)
-// {
-//     char[] wordArray = pangramArray[i].ToCharArray();
-//     Array.Reverse(wordArray);
-//     pangramArray[i] = new string(wordArray);
-// }
+// Console.WriteLine("1234567890123456789012345678901234567890");
+// Console.WriteLine(formattedLine);
 
-// Console.WriteLine(string.Join(" ", pangramArray));
+string customerName = "Ms. Barros";
 
-string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
 
-string[] orderIds = orderStream.Split(",");
-Array.Sort(orderIds);
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
 
-foreach (string orderId in orderIds)
-{
-    Console.Write(orderId);
-    if (orderId.Length != 4)
-        Console.Write("\t - Error");
-    Console.WriteLine();
-}
+// Your logic here
+Console.WriteLine($"Dear {customerName}");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine();
+Console.WriteLine($"Currently, you own ${currentShares:N} shares at a return of {currentReturn:P2}");
+Console.WriteLine();
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C2}");
+Console.WriteLine();
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+string formattedCurrentReturn = string.Format("{0:P2}", currentReturn);
+string formattedCurrentProfit = string.Format("{0:C2}", currentProfit);
+string formattedNewReturn = string.Format("{0:P2}", newReturn);
+string formattedNewProfit = string.Format("{0:C2}", newProfit);
+comparisonMessage += $"{currentProduct.PadRight(17)}{formattedCurrentReturn.PadLeft(10)}{formattedCurrentProfit.PadLeft(17)}\n";
+comparisonMessage += $"{newProduct.PadRight(17)}{formattedNewReturn.PadLeft(10)}{formattedNewProfit.PadLeft(17)}";
+
+Console.WriteLine(comparisonMessage);
