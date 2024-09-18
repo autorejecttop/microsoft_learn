9bfc861701655be866f57cc98fe7e9f1652034df
autorejecttop <autorejecttop@outlook.com>
2024-09-18 13:23:27 +0800

    Guided project - Planned a Petting Zoo Visit

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,188 +1,71 @@
-﻿// // // // // double total = 0;
-// // // // // double minimumSpend = 30.00;
+﻿/*
+    - There will be three visiting schools
+        - School A has six visiting groups (the default number)
+        - School B has three visiting groups
+        - School C has two visiting groups
 
-// // // // // double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
-// // // // // double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+    - For each visiting school, perform the following tasks
+        - Randomize the animals
+        - Assign the animals to the correct number of groups
+        - Print the school name
+        - Print the animal groups
+*/
 
-// // // // // for (int i = 0; i < items.Length; i++)
-// // // // //     total += GetDiscountedPrice(i);
+string[] pettingZoo = [
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+];
 
-// // // // // total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
 
-// // // // // Console.WriteLine($"Total: ${FormatDecimal(total)}");
-
-// // // // // double GetDiscountedPrice(int itemIndex)
-// // // // // {
-// // // // //     // Calculate the discounted price of the item
-// // // // //     return items[itemIndex] * (1 - discounts[itemIndex]);
-// // // // // }
-
-// // // // // bool TotalMeetsMinimum()
-// // // // // {
-// // // // //     // Check if the total meets the minimum
-// // // // //     return total >= minimumSpend;
-// // // // // }
-
-// // // // // string FormatDecimal(double input)
-// // // // // {
-// // // // //     // Format the double so only 2 decimal places are displayed
-// // // // //     return input.ToString()[..5];
-// // // // // }
-
-// // // // double usd = 23.73;
-// // // // int vnd = UsdToVnd(usd);
-
-// // // // Console.WriteLine($"${usd} USD = ${vnd} VND");
-// // // // Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
-
-// // // // int UsdToVnd(double usd)
-// // // // {
-// // // //     int rate = 23500;
-// // // //     return (int)(rate * usd);
-// // // // }
-
-// // // // double VndToUsd(int vnd)
-// // // // {
-// // // //     double rate = 23500;
-// // // //     return vnd / rate;
-// // // // }
-
-// // // string input = "snake";
-
-// // // Console.WriteLine(input);
-// // // Console.WriteLine(ReverseWord(input));
-
-// // // string inputSentence = "there are snakes at the zoo";
-
-// // // Console.WriteLine(inputSentence);
-// // // Console.WriteLine(ReverseSentence(inputSentence));
-
-// // // string ReverseSentence(string input)
-// // // {
-// // //     string result = "";
-// // //     string[] words = input.Split(" ");
-
-// // //     foreach (string word in words)
-// // //     {
-// // //         result += ReverseWord(word) + " ";
-// // //     }
-
-// // //     return result.Trim();
-// // // }
-
-// // // string ReverseWord(string word)
-// // // {
-// // //     string result = "";
-
-// // //     for (int i = word.Length - 1; i >= 0; i--)
-// // //         result += word[i];
-
-// // //     return result;
-// // // }
-
-// // string[] words = ["racecar", "talented", "deified", "tent", "tenet"];
-
-// // Console.WriteLine("Is it a palindrome?");
-// // foreach (string word in words)
-// //     Console.WriteLine($"{word}: {IsPalindrome(word)}");
-
-// // bool IsPalindrome(string word)
-// // {
-// //     int start = 0;
-// //     int end = word.Length - 1;
-
-// //     while (start < end)
-// //     {
-// //         if (word[start] != word[end])
-// //             return false;
-
-// //         start++;
-// //         end--;
-// //     }
-
-// //     return true;
-// // }
-
-// int target = 80;
-// int[] coins = [5, 5, 50, 25, 25, 10, 5];
-// int[,] result = TwoCoins(coins, target);
-
-// if (result.Length == 0)
-// {
-//     Console.WriteLine("No two coins make change");
-// }
-// else
-// {
-//     Console.WriteLine($"Change found at positions:");
-//     for (int i = 0; i < result.GetLength(0); i++)
-//     {
-//         if (result[i, 0] == -1)
-//             break;
-
-//         Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
-//     }
-// }
-
-
-// static int[,] TwoCoins(int[] coins, int target)
-// {
-//     int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
-//     int count = 0;
-
-//     for (int curr = 0; curr < coins.Length; curr++)
-//     {
-//         for (int next = curr + 1; next < coins.Length; next++)
-//         {
-//             if (coins[curr] + coins[next] == target)
-//             {
-//                 result[count, 0] = curr;
-//                 result[count, 1] = next;
-//                 count++;
-//             }
-
-//             if (count == result.GetLength(0))
-//             {
-//                 return result;
-//             }
-//         }
-//     }
-
-//     return count == 0 ? new int[0, 0] : result;
-// }
-
-Random random = new Random();
-
-Console.WriteLine("Would you like to play? (Y/N)");
-if (ShouldPlay())
+void PlanSchoolVisit(string schoolName, int groups = 6)
 {
-    PlayGame();
+    RandomizeAnimals();
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
 }
 
-void PlayGame()
+void RandomizeAnimals()
 {
-    var play = true;
+    Random random = new();
 
-    while (play)
+    for (int i = 0; i < pettingZoo.Length; i++)
     {
-        int target = random.Next(1, 6);
-        int roll = random.Next(1, 7);
+        int r = random.Next(i, pettingZoo.Length);
 
-        Console.WriteLine($"Roll a number greater than {target} to win!");
-        Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose(roll, target));
-        Console.WriteLine("\nPlay again? (Y/N)");
-
-        play = ShouldPlay();
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
     }
 }
 
-string WinOrLose(int roll, int target)
+string[,] AssignGroup(int groups = 6)
 {
-    return roll > target ? "You win!" : "You lose!";
+    string[,] result = new string[groups, pettingZoo.Length / groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
 }
 
-bool ShouldPlay()
+void PrintGroup(string[,] group)
 {
-    string? readResult = Console.ReadLine();
-    return readResult != null && readResult.Trim().ToLower() == "y";
+    for (int i = 0; i < group.GetLength(0); i++)
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < group.GetLength(1); j++)
+            Console.Write($"{group[i, j]} ");
+        Console.WriteLine();
+    }
 }
