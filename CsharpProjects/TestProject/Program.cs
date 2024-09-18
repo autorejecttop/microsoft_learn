a4bc74f98577b6b5818b4a859bffc9dc45172409
autorejecttop <autorejecttop@outlook.com>
2024-09-18 10:21:30 +0800

    Created C# methods with parameters

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,166 +1,196 @@
-﻿// // // void DisplayRandomNumbers()
+﻿// // // // // // // // CountTo(5);
+
+// // // // // // // // void CountTo(int max)
+// // // // // // // // {
+// // // // // // // //     for (int i = 0; i < max; i++)
+// // // // // // // //     {
+// // // // // // // //         Console.Write($"{i}, ");
+// // // // // // // //     }
+// // // // // // // // }
+
+// // // // // // // void DisplayAdjustedTimes(int[] times, int currentGmt, int newGmt)
+// // // // // // // {
+// // // // // // //     int diff = 0;
+
+// // // // // // //     if (Math.Abs(newGmt) > 12 || Math.Abs(currentGmt) > 12)
+// // // // // // //         Console.WriteLine("Invalid GMT");
+// // // // // // //     else if ((newGmt <= 0 && currentGmt <= 0) || (newGmt >= 0 && currentGmt >= 0))
+// // // // // // //         diff = 100 * (Math.Abs(newGmt) - Math.Abs(currentGmt));
+// // // // // // //     else
+// // // // // // //         diff = 100 * (Math.Abs(newGmt) + Math.Abs(currentGmt));
+
+// // // // // // //     for (int i = 0; i < times.Length; i++)
+// // // // // // //     {
+// // // // // // //         int newTime = (times[i] + diff) % 2400;
+// // // // // // //         Console.WriteLine($"{times[i]} -> {newTime}");
+// // // // // // //     }
+// // // // // // // }
+
+// // // // // // // int[] schedule = [800, 1200, 1600, 2000];
+
+// // // // // // // DisplayAdjustedTimes(schedule, 6, -6);
+
+// // // // // // string[] students = ["Jenna", "Ayesha", "Carlos", "Viktor"];
+
+// // // // // // DisplayStudents(students);
+// // // // // // DisplayStudents(["Robert", "Vanya"]);
+
+// // // // // // void DisplayStudents(string[] students)
+// // // // // // {
+// // // // // //     foreach (string student in students)
+// // // // // //     {
+// // // // // //         Console.Write($"{student}, ");
+// // // // // //     }
+// // // // // //     Console.WriteLine();
+// // // // // // }
+
+// // // // // double pi = 3.14159;
+// // // // // PrintCircleInfo(12);
+// // // // // PrintCircleInfo(24);
+
+// // // // // void PrintCircleInfo(int radius)
+// // // // // {
+// // // // //     Console.WriteLine($"Circle with radius {radius}");
+// // // // //     PrintCircleArea(radius);
+// // // // //     PrintCircleCircumference(radius);
+// // // // // }
+
+// // // // // void PrintCircleArea(int radius)
+// // // // // {
+// // // // //     double area = pi * (radius * radius);
+// // // // //     Console.WriteLine($"Area = {area}");
+// // // // // }
+
+// // // // // void PrintCircleCircumference(int radius)
+// // // // // {
+// // // // //     double circumference = 2 * pi * radius;
+// // // // //     Console.WriteLine($"Circumference = {circumference}");
+// // // // // }
+
+// // // // int a = 3;
+// // // // int b = 4;
+// // // // int c = 0;
+
+// // // // Multiply(a, b, c);
+// // // // Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+// // // // void Multiply(int a, int b, int c)
+// // // // {
+// // // //     c = a * b;
+// // // //     Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+// // // // }
+
+// // // int[] array = [1, 2, 3, 4, 5];
+
+// // // PrintArray(array);
+// // // Clear(array);
+// // // PrintArray(array);
+
+// // // void PrintArray(int[] array)
 // // // {
-// // //     Random random = new();
-
-// // //     for (int i = 0; i < 5; i++)
-// // //         Console.Write($"{random.Next(1, 100)} ");
-
+// // //     foreach (int a in array)
+// // //     {
+// // //         Console.Write($"{a} ");
+// // //     }
 // // //     Console.WriteLine();
 // // // }
 
-// // // Console.WriteLine("Generating random numbers:");
-// // // DisplayRandomNumbers();
+// // // void Clear(int[] array)
+// // // {
+// // //     for (int i = 0; i < array.Length; i++)
+// // //     {
+// // //         array[i] = 0;
+// // //     }
+// // // }
 
-// // int[] times = [800, 1200, 1600, 2000];
-// // int diff = 0;
+// // string status = "Healthy";
 
-// // Console.WriteLine("Enter current GMT");
-// // int currentGMT = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine($"Start: {status}");
+// // SetHealth(false);
+// // Console.WriteLine($"End: {status}");
 
-// // Console.WriteLine("Current Medicine Schedule:");
-// // DisplayTimes();
-
-// // Console.WriteLine("Enter new GMT");
-// // int newGMT = Convert.ToInt32(Console.ReadLine());
-
-// // if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+// // void SetHealth(bool isHealthy)
 // // {
-// //     Console.WriteLine("Invalid GMT");
-// // }
-// // else if ((newGMT <= 0 && currentGMT <= 0) || (newGMT >= 0 && currentGMT >= 0))
-// // {
-// //     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-// //     AdjustTimes();
-// // }
-// // else
-// // {
-// //     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-// //     AdjustTimes();
+// //     status = isHealthy ? "Healthy" : "Unhealthy";
+// //     Console.WriteLine($"Middle: {status}");
 // // }
 
-// // Console.WriteLine("New Medicine Schedule:");
-// // DisplayTimes();
+// string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+// string[] rsvps = new string[10];
+// int count = 0;
 
-// // Console.WriteLine();
+// RSVP("Rebecca");
+// RSVP("Nadia", 2, "Nuts");
+// RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+// RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+// RSVP("Noor", 4, inviteOnly: false);
+// RSVP("Jonte", 2, "Stone fruit", false);
+// ShowRSVPs();
 
-
-// // void DisplayTimes()
-// // {
-// //     /* Format and display medicine times */
-// //     foreach (int val in times)
-// //     {
-// //         string time = val.ToString();
-// //         int len = time.Length;
-
-// //         if (len >= 3)
-// //         {
-// //             time = time.Insert(len - 2, ":");
-// //         }
-// //         else if (len == 2)
-// //         {
-// //             time = time.Insert(0, "0:");
-// //         }
-// //         else
-// //         {
-// //             time = time.Insert(0, "0:0");
-// //         }
-
-// //         Console.Write($"{time} ");
-// //     }
-
-// //     Console.WriteLine();
-// // }
-
-// // void AdjustTimes()
-// // {
-// //     /* Adjust the times by adding the difference, keeping the value within 24 hours */
-// //     for (int i = 0; i < times.Length; i++)
-// //     {
-// //         times[i] = (times[i] + diff) % 2400;
-// //     }
-// // }
-
-// /*
-//     if ipAddress consists of 4 numbers
-//     and
-//     if each ipAddress number has no leading zeroes
-//     and
-//     if each ipAddress numebr is in range 0 - 255
-
-//     then ipAddress is valid
-
-//     else ipAddress is invalid
-// */
-// string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
-// string[] address;
-// bool validLength = false;
-// bool validZeroes = false;
-// bool validRange = false;
-
-// foreach (string ip in ipv4Input)
+// void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
 // {
-//     address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
-
-//     ValidateLength();
-//     ValidateZeroes();
-//     ValidateRange();
-
-//     if (validLength && validZeroes && validRange)
-//         Console.WriteLine($"{ip} is a valid IPv4 address");
-//     else
-//         Console.WriteLine($"{ip} is an invalid IPv4 address");
-// }
-
-// void ValidateLength()
-// {
-//     validLength = address.Length == 4;
-// }
-
-// void ValidateZeroes()
-// {
-//     foreach (string number in address)
+//     if (inviteOnly)
 //     {
-//         if (number.Length > 1 && number.StartsWith("0"))
+//         // search guestList before adding rsvp
+//         bool found = false;
+
+//         foreach (string guest in guestList)
 //         {
-//             validZeroes = false;
+//             if (guest.Equals(name))
+//             {
+//                 found = true;
+//                 break;
+//             }
+//         }
+
+//         if (!found)
+//         {
+//             Console.WriteLine($"Sorry, {name} is not on the guest list");
 //             return;
 //         }
 //     }
 
-//     validZeroes = true;
+//     rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+//     count++;
 // }
 
-// void ValidateRange()
+// void ShowRSVPs()
 // {
-//     foreach (string number in address)
+//     Console.WriteLine("\nTotal RSVPs:");
+//     for (int i = 0; i < count; i++)
 //     {
-//         int value = int.Parse(number);
-//         if (value < 0 || value > 255)
-//         {
-//             validRange = false;
-//             return;
-//         }
+//         Console.WriteLine(rsvps[i]);
 //     }
-
-//     validRange = true;
 // }
 
-Random random = new();
-int luck = random.Next(100);
-
-string[] text = ["You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"];
-string[] good = ["look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"];
-string[] bad = ["fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."];
-string[] neutral = ["appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."];
-
-TellFortune();
-
-void TellFortune()
+string[,] corporate =
 {
-    Console.WriteLine("A fortune teller whispers the following words:");
-    string[] fortune = luck > 75 ? good : (luck < 25 ? bad : neutral);
-    for (int i = 0; i < 4; i++)
-    {
-        Console.Write($"{text[i]} {fortune[i]} ");
-    }
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    // display internal email addresses
+    DisplayEmailAddress(firstName: corporate[i, 0], lastName: corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    // display external email addresses
+    DisplayEmailAddress(firstName: external[i, 0], lastName: external[i, 1], domain: externalDomain);
+}
+
+void DisplayEmailAddress(string firstName, string lastName, string domain = "contoso.com")
+{
+    Console.WriteLine($"{firstName[..2]}{lastName}@{domain}".ToLower());
 }
